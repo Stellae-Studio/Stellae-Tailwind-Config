057d6b60f9373d796277a165847279edf77c5d76
Nikolas Prosenik <nikolas.prosenik@gmail.com>
2022-05-09 09:55:15 +0200

    update readme, added config

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  purge: [
+    "./pages/*.{js,jsx,ts,tsx}",
+    "./components/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  mode: "jit",
+  darkMode: "class", // or 'media' or 'class'
+  content: ["./pages/*.{js,jsx,ts,tsx}", "./components/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
+    },
+    extend: {
+      screens: {
+        mf: "990px",
+      },
+      keyframes: {
+        "slide-in": {
+          "0%": {
+            "-webkit-transform": "translateX(120%)",
+            transform: "translateX(120%)",
+          },
+          "100%": {
+            "-webkit-transform": "translateX(0%)",
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "slide-in": "slide-in 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"), 
+    require("@tailwindcss/forms")],
+};
